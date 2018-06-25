be2d92384e8bb87c61d46c4ea55331b2f89da248
Than Htike Aung <rage.cataclysm>
2018-06-25 22:54:48 +0630

    start created blank project

---
+++ src/main/java/com/github/cataclysmuprising/springdatajpatutorial/SpringDataJpaTutorialApplication.java
@@ -0,0 +1,12 @@
+package com.github.cataclysmuprising.springdatajpatutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaTutorialApplication.class, args);
+	}
+}
