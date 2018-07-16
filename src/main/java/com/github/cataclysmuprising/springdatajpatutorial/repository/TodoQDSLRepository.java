c1e4301dd22aa7504b35bb1f5b930434d0eff8a7
Than Htike Aung <rage.cataclysm>
2018-07-16 21:34:33 +0630

    Test for rest query language

---
+++ src/main/java/com/github/cataclysmuprising/springdatajpatutorial/repository/TodoQDSLRepository.java
@@ -0,0 +1,20 @@
+package com.github.cataclysmuprising.springdatajpatutorial.repository;
+
+import com.github.cataclysmuprising.springdatajpatutorial.entity.QTodo;
+import com.github.cataclysmuprising.springdatajpatutorial.entity.Todo;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.types.dsl.StringPath;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.SingleValueBinding;
+
+public interface TodoQDSLRepository extends JpaRepository<Todo, Long>,
+		QuerydslPredicateExecutor<Todo>, QuerydslBinderCustomizer<QTodo> {
+	@Override
+	default void customize(
+			QuerydslBindings bindings, QTodo root) {
+		bindings.bind(String.class).first((SingleValueBinding<StringPath, String>) StringExpression::containsIgnoreCase);
+	}
+}
